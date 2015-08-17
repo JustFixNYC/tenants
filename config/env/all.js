a9beacb020249e848924f897f75aa048934bf23c
Dan Kass <romeboards@gmail.com>
2015-08-17 11:54:21 -0400

    ok putting env files back in

---
+++ config/env/all.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+	app: {
+	  title: 'JustFix.nyc',
+	  description: 'JustFix.nyc is a tool for New Yorkers to document, organize, and take action in getting repairs made on your apartment.',
+	  keywords: 'something'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+	  lib: {
+		  css: [
+		    // 'public/lib/bootstrap/dist/css/bootstrap.css',
+		    // 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+		    'lib/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.css',
+		    'public/styles/bootstrap.css',
+		    'public/styles/bootflat.css'
+		  ],
+		  js: [
+		    'public/lib/angular/angular.js',
+		    'public/lib/angular-resource/angular-resource.js', 
+		    'public/lib/angular-animate/angular-animate.js', 
+		    'public/lib/angular-ui-router/release/angular-ui-router.js',
+		    'public/lib/angular-touch/angular-touch.min.js',
+		    'public/lib/angular-ui-utils/ui-utils.js',
+		    'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+		    'public/lib/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.min.js',
+		    'public/lib/re-tree/re-tree.min.js',
+		    'public/lib/ng-device-detector/ng-device-detector.min.js',
+		    'public/lib/ng-file-upload-shim/ng-file-upload-shim.min.js',
+		    'public/lib/ng-file-upload/ng-file-upload.min.js'
+		  ]
+	  },
+	  css: [
+	    'public/styles/style.css'
+	  ],
+	  js: [
+		  'public/config.js',
+		  'public/application.js',
+		  'public/modules/*/*.js',
+		  'public/modules/*/*[!tests]*/*.js'
+	  ],
+	  tests: [
+	    'public/lib/angular-mocks/angular-mocks.js',
+	    'public/modules/*/tests/*.js'
+	  ]
+	},
+	geoclient: {
+	  id: process.env.GEOCLIENT_APP_ID,
+	  key: process.env.GEOCLIENT_APP_KEY,
+	  url: process.env.GEOCLIENT_URL
+	},
+	aws: {
+		id: process.env.AWS_ACCESS_KEY_ID,
+		key: process.env.AWS_SECRET_ACCESS_KEY
+	}     
+};
+
---
+++ config/env/production.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/trowel',
+  assets: {
+    lib: {
+      css: [
+      ],
+      js: [
+        'public/lib/angular/angular.min.js',
+        'public/lib/angular-resource/angular-resource.js', 
+        'public/lib/angular-animate/angular-animate.js', 
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+      ]
+    },
+    css: 'public/dist/style.min.css',
+    js: 'public/dist/application.min.js'
+  },
+  mailer: {
+    from: process.env.MAILER_FROM || 'MAILER_FROM',
+    options: {
+      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+      auth: {
+        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+      }
+    }
+  }
+};
