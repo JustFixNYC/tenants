fbe21447deb3404b8c4dc1654f3264090947cba0
Dan Kass <romeboards@gmail.com>
2016-04-29 11:34:10 -0400

    starting to refactor

--- public/modules/onboarding/config/onboarding.client.config.js
+++ public/modules/onboarding/config/onboarding.client.config.js
@@ -1,15 +1,15 @@
-/*(function() {
-  'use strict';
+'use strict';
 
-  // Onboarding module config
-  angular
-    .module('onboarding')
-    .run(menuConfig);
+angular.module('onboarding').run(['$rootScope', '$state', 'Authentication', '$window', function($rootScope, $state, Authentication, $window) {
 
-  menuConfig.$inject = ['Menus'];
+	$rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    console.log(toState.name);
+		// if(destination.data && destination.data.needAdmin === true && Authentication.user.roles[0].toLowerCase() !== 'admin') {
+		//if(de) {
+			// e.preventDefault();
+			// $window.location.href = '#!/home';
+			//console.log('this should not be visible');
+		//}
+	});
 
-  function menuConfig(Menus) {
-    // Config logic
-    // ...
-  }
-})();*/
+}]);
--- public/modules/onboarding/config/onboarding.client.routes.js
+++ public/modules/onboarding/config/onboarding.client.routes.js
@@ -1,58 +1,41 @@
 'use strict';
 
 //Setting up route
-angular.module('onboarding').config(['$stateProvider', function($stateProvider){
+angular.module('onboarding').config(['$stateProvider', '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider){
 
-	// Onboarding state routing
-  $stateProvider
-    .state('onboarding', {
-      url: '/onboarding',
-      templateUrl: 'modules/onboarding/views/onboarding.client.view.html',
-      controller: 'OnboardingController'
-    })
-    .state('onboarding-create', {
-      url: '/onboarding-admin',
-      templateUrl: 'modules/onboarding/views/onboarding-create.client.view.html',
-      controller: 'OnboardingCreateController',
-      controllerAs: 'vm',
-      data: {
-      	needAdmin: true
-      }
-    })/*
-    .state('selection', {
-      url: '/onboarding-selection',
-      templateUrl: 'modules/onboarding/views/onboarding-selection.client.view.html',
-      controller: 'OnboardingSelectionController',
-      controllerAs: 'vm'
-    })*/
-    .state('onboarding-problems', {
-      url: '/onboarding-problems',
-      templateUrl: 'modules/problems/views/onboarding-problems.client.view.html',
-      controller: 'OnboardingProblemsController',
-      controllerAs: 'vm'
-    })
-    .state('onboarding-details', {
-      url: '/onboarding-details',
-      templateUrl: 'modules/users/views/onboarding-details.client.view.html',
-      controller: 'OnboardingDetailsController',
-      controllerAs: 'vm'
-    })
+    // Jump to first child state
+    $urlRouterProvider.when('/onboarding', '/onboarding/code');
+
+  	// Onboarding state routing
     $stateProvider
-      .state('onboarding-tutorial', {
-        url: '/onboarding-tutorial',
-        templateUrl: 'modules/onboarding/views/onboarding-tutorial.client.view.html',
-        controller: 'OnboardingTutorialController',
-        controllerAs: 'vm'
+      .state('onboarding', {
+        url: '/onboarding',
+        templateUrl: 'modules/onboarding/views/onboarding.client.view.html',
+        controller: 'OnboardingController',
+        abstract: true
+      })
+      .state('onboarding.accessCode', {
+        url: '/code',
+        templateUrl: 'modules/onboarding/partials/onboarding-code.client.view.html'
+      })
+      .state('onboarding.problems', {
+        url: '/checklist',
+        templateUrl: 'modules/onboarding/partials/onboarding-problems.client.view.html'
+      })
+      .state('onboarding.details', {
+        url: '/personal',
+        templateUrl: 'modules/onboarding/partials/onboarding-details.client.view.html'
+      })
+      .state('onboarding.tutorial', {
+        url: '/tutorial',
+        templateUrl: 'modules/onboarding/partials/onboarding-tutorial.client.view.html'
       });
+    // $stateProvider
+    //   .state('onboarding-tutorial', {
+    //     url: '/onboarding-tutorial',
+    //     templateUrl: 'modules/onboarding/views/onboarding-tutorial.client.view.html',
+    //     controller: 'OnboardingTutorialController',
+    //     controllerAs: 'vm'
+    //   });
 }]);
-angular.module('onboarding').run(['$rootScope', '$state', 'Authentication', '$window', function($rootScope, $state, Authentication, $window) {
-
-	$rootScope.$on('$stateChangeStart', function(e, destination) {
-		if(destination.data && destination.data.needAdmin === true && Authentication.user.roles[0].toLowerCase() !== 'admin') {
-			// e.preventDefault();
-			// $window.location.href = '#!/home';
-			console.log('this should not be visible');
-		}
-	})
-
-}])
