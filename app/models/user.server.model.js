4630345f4a71ec3b408e273b4df3bc0bb5192021
meegan <matt.l.egan@gmail.com>
2016-05-17 14:50:48 -0400

    remove email, text update

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -45,11 +45,6 @@ var UserSchema = new Schema({
     trim: true,
     default: ''
   },
-  /*email: {
-  	type: String,
-  	default: '',
-  	unique: true
-  },*/
   borough: {
     type: String,
     default: ''
