3e531fd5ab22466f878048c4610e99a240e24df3
meegan <matt.l.egan@gmail.com>
2016-05-23 22:29:19 -0400

    Clean up KYR, remove X icon on probs modal

--- public/modules/kyr/config/kyr.client.routes.js
+++ public/modules/kyr/config/kyr.client.routes.js
@@ -6,20 +6,26 @@
     .module('kyr')
     .config(routeConfig);
 
-  routeConfig.$inject = ['$stateProvider'];
+  routeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-  function routeConfig($stateProvider) {
+  function routeConfig($stateProvider, $urlRouterProvider) {
     // Kyr state routing
     $stateProvider
       .state('kyr', {
         url: '/kyr',
         templateUrl: 'modules/kyr/views/kyr.client.view.html',
-        controller: 'KyrController'
+        controller: 'KyrController',
+        noMargin: true
       })
       .state('kyrDetail', {
       	url: '/kyr/:kyrId',
       	templateUrl: 'modules/kyr/views/kyr-detail.client.view.html',
-      	controller: 'KyrDetailController'
+      	controller: 'KyrDetailController',
+      	noMargin: true,
+				data: {
+					disableBack: true
+				},
+				localHistory: true
       });
   }
 })();
