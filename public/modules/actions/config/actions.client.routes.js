4ac0a005723060fde42550726ba583955c938af4
Dan Kass <romeboards@gmail.com>
2016-08-22 13:37:44 -0400

    removing all legacy issues code

--- public/modules/actions/config/actions.client.routes.js
+++ public/modules/actions/config/actions.client.routes.js
@@ -4,10 +4,8 @@
 angular.module('actions').config(['$stateProvider', '$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 
-		// Jump to first child state
-		//$urlRouterProvider.when('/issues/create', '/issues/create/checklist');
 
-		// Issues state routing
+		// Actions state routing
 		$stateProvider
 			.state('listActions', {
 				url: '/take-action',
