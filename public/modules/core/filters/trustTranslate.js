806c2d8152a113098bedfd9b74090e49790a760a
meegan <matt.l.egan@gmail.com>
2016-08-22 15:56:44 -0400

    Remove single translation bindings

--- public/modules/core/filters/trustTranslate.js
+++ public/modules/core/filters/trustTranslate.js
@@ -4,6 +4,7 @@ angular.module('core').filter('trustTranslate', ['$sce', '$filter',
 	function($sce, $filter) {
 		var translatedText = $filter('translate');
     return function (val) {
+    	console.log(val);
     	return $sce.trustAsHtml(translatedText(val));
     }
 }]);
