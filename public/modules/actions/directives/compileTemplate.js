2c74bb092430728350e83f3a1f7bbe0781d615e7
meegan <matt.l.egan@gmail.com>
2016-08-25 15:36:58 -0400

    attempting fix for translations service compile bug

--- public/modules/actions/directives/compileTemplate.js
+++ public/modules/actions/directives/compileTemplate.js
@@ -4,8 +4,8 @@
 // see http://stackoverflow.com/questions/20297638/call-function-inside-sce-trustashtml-string-in-angular-js
 // TODO: this is used all over the place, move it into /core module?
 
-angular.module('actions').directive('compileTemplate', ['$compile', '$parse', '$sce',
-	function($compile, $parse, $sce){
+angular.module('actions').directive('compileTemplate', ['$compile', '$parse', '$sce', '$translate',
+	function($compile, $parse, $sce, $translate){
     return {
         link: function(scope, element, attr){
             var parsed = $parse(attr.ngBindHtml);
@@ -13,8 +13,16 @@ angular.module('actions').directive('compileTemplate', ['$compile', '$parse', '$
             var getStringValue = function() { return (parsed(scope) || '').toString(); }
             //Recompile if the template changes
             scope.$watch(getStringValue, function(val) {
-              $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
-            });
+            	// Check if our translation service has failed
+            	if(val.indexOf('modules') !== -1) {
+            		$translate(val).then(function(newVal){
+	            		element.html(newVal);
+	            		$compile(element, null, -9999)(scope);	
+            		});
+            	} else {
+	              $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
+            	}
+          	});
         }
     }
 	}
--- public/modules/core/filters/trustTranslate.js
+++ public/modules/core/filters/trustTranslate.js
@@ -3,9 +3,8 @@
 angular.module('core').filter('trustTranslate', ['$sce', '$filter',
 	function($sce, $filter) {
 		var translatedText = $filter('translate');
-    return function (val) {
+	  return function (val) {
     	var returnedTranslation = translatedText(val);
-    	console.log(returnedTranslation);
     	return $sce.trustAsHtml(returnedTranslation);
     }
 }]);
