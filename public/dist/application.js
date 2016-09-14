f42048e59baca7ab87b0580c6e2ac4f17b59dad2
Dan Kass <romeboards@gmail.com>
2016-09-14 15:24:00 -0700

    donate buttons and manifesto update

--- public/dist/application.js
+++ public/dist/application.js
@@ -1433,6 +1433,18 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider', '$provide
 
 'use strict';
 
+
+angular.module('core').controller('ContactController', ['$rootScope', '$scope', 'Authentication', 'deviceDetector',
+	function($rootScope, $scope, Authentication, deviceDetector) {
+		// This provides Authentication context.
+		$scope.authentication = Authentication;
+    $scope.device = deviceDetector;
+
+	}
+]);
+
+'use strict';
+
 angular.module('core').controller('FooterController', ['$scope', '$window', 'Authentication',
   function($scope, $window, Authentication) {
 
