f28c22aeb39cd3eeca0af5a1eb60b7df1f9a7afe
Dan Kass <romeboards@gmail.com>
2016-10-19 14:08:41 -0400

    recompiling

--- public/dist/application.js
+++ public/dist/application.js
@@ -129,7 +129,6 @@ angular.module(ApplicationConfiguration.applicationModuleName)
     // ensure that this happens on pageload
     // https://github.com/angular-ui/ui-router/issues/1307
     var setHeaderState = function(name) {
-      console.log('name', name);
       switch(name) {
         case 'landing':
           $rootScope.headerInner = false;
@@ -151,8 +150,6 @@ angular.module(ApplicationConfiguration.applicationModuleName)
 
       $rootScope.state = toState.name;
 
-      console.log('to state', toState.name);
-
       if(toState.data && toState.data.disableBack) {
         $rootScope.showBack = false;
       } else {
@@ -170,24 +167,24 @@ angular.module(ApplicationConfiguration.applicationModuleName)
     var userLang = navigator.language || navigator.userLanguage;
 
     if(!$location.search().hasOwnProperty('lang')) { // No language selected, check if browser lang is true
-      console.log('condition 0');
+      // console.log('condition 0');
     	if(LocaleService.checkIfLocaleIsValid(userLang)) {
     		LocaleService.setLocaleByName(userLang);
     	}
     	return;
   	} else if(langQuery === 'es' || langQuery === 'es-mx') { // Spanish URL slightly wrong
-      console.log('condition 1');
+      // console.log('condition 1');
 			$location.search('lang', 'es_mx');
 			LocaleService.setLocaleByName('es_mx');
 		}else if(langQuery === 'en' || langQuery === 'en-us') { // English url slightly wrong
-      console.log('condition 2');
+      // console.log('condition 2');
 			$location.search('lang', 'en_US');
 			LocaleService.setLocaleByName('en_US');
 		} else if(LocaleService.checkIfLocaleIsValid(langQuery)){  // account for exactly-correct urls
-      console.log('condition 3');
+      // console.log('condition 3');
 			LocaleService.setLocaleByName(langQuery);
 		} else { 														// Totally wrong lang query, default to english
-      console.log('condition 4');
+      // console.log('condition 4');
 			$location.search('lang', '');
   	}
   }]);
@@ -2086,15 +2083,30 @@ angular.module('core')
       scope: false,
       link: function (scope, element, attrs) {
 
-        var isIOS8 = function() {
-          var deviceAgent = deviceDetector.raw.userAgent.toLowerCase();
-          return /(iphone|ipod|ipad).* os [8-9]_/.test(deviceAgent);
+
+        var getIOSversion = function() {
+          // if (/iP(hone|od|ad)/.test(navigator.platform)) {
+            // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
+            var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
+            return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
+          // } else {
+            // this shouldn't happen
+            // Rollbar.error("Didn't detect iOS correctly?");
+          // }
+        }
+
+        var isGtIOS7 = function() {
+          if (deviceDetector.os === 'ios' && getIOSversion()[0] > 7) {
+            return true;
+          } else {
+            return false;
+          }
         };
 
 
-        // ios ;
-        // ios8 &
-        // android ?
+        // ios <=7  ;
+        // ios >7   &
+        // android  ?
 
 
         element.on('click', function (e) {
@@ -2108,7 +2120,7 @@ angular.module('core')
           }
 
           if(deviceDetector.os === 'ios') {
-            if(isIOS8()) href += '&';
+            if(isGtIOS7()) href += '&';
             else href += ';';
             href = href + 'body=' + msg;
             console.log('href', href);
