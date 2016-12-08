5da6957dca53838a291a96cf8156b833c0a22d7a
meegan <matt.l.egan@gmail.com>
2016-12-08 12:52:51 -0500

    fix for missing css/js

--- public/dist/application.js
+++ public/dist/application.js
@@ -1201,6 +1201,8 @@ angular.module('activity').controller('ActivityController', ['$scope', '$locatio
 angular.module('activity').controller('PrintController', ['$scope', '$rootScope', '$filter', 'Activity', 'Lightbox', 'Authentication', '$window',
   function($scope, $rootScope, $filter, Activity, Lightbox, Authentication, $window) {
 
+  	$scope.printable = false;
+
     $scope.list = function() {
     	var photoOrder = 0;
 
@@ -1235,6 +1237,11 @@ angular.module('activity').controller('PrintController', ['$scope', '$rootScope'
       var createdDate = new Date(created).setHours(0,0,0,0);
       return startDate !== createdDate;
     }
+
+    $rootScope.$on('$viewContentLoaded', function() {
+    	$scope.printable = true;
+    	console.log('displayable!');
+    });
 
 	}
 ]);
@@ -2217,8 +2224,8 @@ angular.module('core').directive('phoneInput', ["$filter", "$browser", function(
 'use strict';
 
 angular.module('core')
-  .directive('printButton', ['deviceDetector', '$window', '$http',
-  function (deviceDetector, $window, $http) {
+  .directive('printButton', ['deviceDetector', '$window', '$timeout', '$rootScope',
+  function (deviceDetector, $window, $timeout, $rootScope) {
     return {
       restrict: 'A',
       scope: false,
@@ -2228,20 +2235,54 @@ angular.module('core')
           element.css("display", "none");
         } else {
           element.addClass("print-button");
+          element.addClass('disabled');
         }
 
         var printPg = document.createElement('iframe');
         printPg.src = '/print';
         printPg.width = 700;
-        printPg.height = 100;
+        printPg.height = 0;
         printPg.name = 'frame';
         document.body.appendChild(printPg);
 
+        // Listen for when iFrame is loaded (safety measure so we KNOW the attached will load correctly)
         printPg.onload = function() {
 
-	        element.on('click', function (event) {
-				    window.frames['frame'].print();
-	        });
+	        // need to access scope INSIDE the iframe, so we can trust angular to tell us when we're fully loaded instead of parent JS
+	        var printView = printPg.contentWindow.angular.element(printPg.contentWindow.document.querySelector('#print-view'));
+	        console.log(printView);
+
+	        var checkLoaded = function(){
+	        	var printView = printPg.contentWindow.angular.element(printPg.contentWindow.document.querySelector('#print-view'));
+	        	if(!printView.scope()) {
+	        		return $timeout(function(){
+	        			console.log('huh?');
+	        			// Recusive functions FTW
+		        		checkLoaded();
+	        		}, 500);
+	        	}
+	        	var printableLoaded = printView.scope().printable;
+
+	        	// Check if we're actually loaded
+	        	if(!printableLoaded) {
+	        		$timeout(function(){
+	        			console.log('huh?');
+	        			// Recusive functions FTW
+		        		checkLoaded();
+	        		}, 500);
+	        	} else {
+	        		// If we are loaded, let this button be free! 
+		        	element.removeClass('disabled');
+
+			        element.on('click', function (event) {
+						    window.frames['frame'].print();
+			        });
+
+	        	}
+	        };
+
+	        checkLoaded();
+
         };
 
 
