bda50852839847df361460b2a76b59419f4cb9d1
meegan <matt.l.egan@gmail.com>
2016-12-29 14:35:37 -0500

    remove console log, add comments

--- public/modules/activity/controllers/print.client.controller.js
+++ public/modules/activity/controllers/print.client.controller.js
@@ -5,8 +5,7 @@ angular.module('activity').controller('PrintController', ['$scope', '$rootScope'
   function($scope, $rootScope, $filter, Activity, Authentication, $state) {
 
   	$scope.printable = false;
-    $scope.user = Authentication.user; // TODO: use 'user' to determine user status, see if we can remove the URL query params
-    console.log($scope.user);
+    $scope.user = Authentication.user;
 
   	// If we need to reload view (should be fired in parent)
   	$scope.reloadView = function() {
@@ -36,12 +35,13 @@ angular.module('activity').controller('PrintController', ['$scope', '$rootScope'
     	if($scope.user){
     		// if we have a logged in user, the activity array will be updated, and we can return that saved val
 				$scope.activities = dataTagAndOrder($scope.user.activity);
-
 	    } else {
 
-		  	// handle publically accessible view w/ key (Possible to do this with access key in url query?)
+		  	// handle publically accessible view w/ URL
+		  	// TODO: figure out how to pass key from parent pg controller, to directive, into the iframe? ($state.params returns print, not parent params)
 		  	var url = (window.location != window.parent.location) ? document.referrer : document.location.href;
 
+		  	// Safety check, if we aren't logged in and are querying for the key then something's gone horribly wrong
 		  	if(url.indexOf('/share/') > -1) {
 		  		var key = {
 		  			key: url.slice(url.lastIndexOf('/') + 1, url.length)
@@ -49,8 +49,10 @@ angular.module('activity').controller('PrintController', ['$scope', '$rootScope'
 		  	} else {
 		  		console.log('Error! No public key available. Please contact us at help@justfix.nyc');
 		  	}
+
 	    	// if we have a key, we'll need to query via the 'public' method on the Activity service (diff query params)
 	    	Activity.public(key, function(data) {
+	    		$scope.user = data;
 	    		$scope.activities = dataTagAndOrder(data.activity);
 	    	}, function(error) {
 	    		console.log(error);
