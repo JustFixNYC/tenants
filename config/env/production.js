cf4640bbcf828f15d80e82713418bbe62c7c2188
Dan Kass <sayheytodan@gmail.com>
2017-02-08 23:06:28 -0500

    attempt 2 at update env refs

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: process.env.MONGODB_URL || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/trowel',
+  db:  process.env.MONGODB_URL || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/trowel',
   app: {
     title: 'JustFix.nyc'
   },
