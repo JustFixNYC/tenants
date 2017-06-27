d5bdaead2e219ef24d74b5bc8fcad56c9573204b
Dan Kass <romeboards@gmail.com>
2017-06-27 11:45:13 -0400

    prod env key update

--- config/env/all.js
+++ config/env/all.js
@@ -89,5 +89,8 @@ module.exports = {
 		accountSid: process.env.TWILIO_ACCOUNT_SID,
 		authToken: process.env.TWILIO_AUTH_TOKEN,
 		twilioNumber: process.env.TWILIO_NUMBER
+	},
+	sendgrid: {
+		apiKey: process.env.SENDGRID_API_KEY
 	}
 };
