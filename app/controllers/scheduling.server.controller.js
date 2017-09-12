3e7c9ae8eecc1a0adcd8d3dc898cef107e00a874
Dan Kass <romeboards@gmail.com>
2017-09-12 11:45:59 -0400

    assert tenant id in scheduling webbook

--- app/controllers/scheduling.server.controller.js
+++ app/controllers/scheduling.server.controller.js
@@ -14,39 +14,48 @@ exports.bookDate = function (req, res) {
   var consultationForm = _.find(req.body.appt.forms, { id: 602488 }).values;
   var tenantID = _.find(consultationForm, { fieldID: 3389743 }).value;
 
-  Tenant.findById(tenantID)
-  // Tenant.findOne({ phone: req.body.appt.phone })
-    .then(function (tenant) {
+  if(tenantID && tenantID !== '') {
+    Tenant.findById(tenantID)
+    // Tenant.findOne({ phone: req.body.appt.phone })
+      .then(function (tenant) {
 
-      if(!tenant) throw new Error("No Tenant Found");
+        if(!tenant) throw new Error("No Tenant Found");
 
-      if(tenant.advocateRole === 'none') {
-        tenant.advocateRole = req.body.advocateRole;
-        tenant.advocate = req.body.advocate;
-        tenant.sharing.enabled = true;
-      }
+        if(tenant.advocateRole === 'none') {
+          tenant.advocateRole = req.body.advocateRole;
+          tenant.advocate = req.body.advocate;
+          tenant.sharing.enabled = true;
+        }
 
-      if(_.contains(tenant.actionFlags, 'scheduleLater')) {
-        _.pull(tenant.actionFlags, 'scheduleLater');
+        if(_.contains(tenant.actionFlags, 'scheduleLater')) {
+          _.pull(tenant.actionFlags, 'scheduleLater');
 
-        // need to notify of array change
-        // http://stackoverflow.com/a/13350955/991673
-        tenant.markModified('actionFlags');
-      }
+          // need to notify of array change
+          // http://stackoverflow.com/a/13350955/991673
+          tenant.markModified('actionFlags');
+        }
 
-      tenant.currentAcuityEventId = req.body.id;
+        tenant.currentAcuityEventId = req.body.id;
 
-      return tenant.save();
-    })
-    .then(function () {
+        return tenant.save();
+      })
+      .then(function () {
 
-      rollbar.reportMessage("New Appointment Scheduled!", "info", req);
-      res.send({
-        message: 'Successfully Updated Appt Id'
+        rollbar.reportMessage("New Appointment Scheduled!", "info", req);
+        res.send({
+          message: 'Successfully Updated Appt Id'
+        });
+      })
+      .catch(function (err) {
+        rollbar.handleErrorWithPayloadData("Scheduling Error", { phone: req.body.appt.phone, error: err }, req);
+        res.status(500).send({ message: errorHandler.getErrorMessage(err) });
       });
-    })
-    .catch(function (err) {
-      rollbar.handleErrorWithPayloadData("Scheduling Error", { phone: req.body.appt.phone, error: err }, req);
-      res.status(500).send({ message: errorHandler.getErrorMessage(err) });
+  } else {
+    rollbar.reportMessage("Scheduling webhook didn't find an ID - probably rescheduled?", "info", req);
+    res.send({
+      message: 'No tenant ID provided'
     });
+  }
+
+
 };
